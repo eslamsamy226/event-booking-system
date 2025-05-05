ddec4fa89841772a9ea1ff3022d7bb241c1486a0
Eslam Samy <eslamsamy226@yahoo.com>
2025-05-05 22:12:05 +0300

    added remote database connection and docker files

--- event-booking-system-backend/src/main/java/com/areeb/event_booking_system_backend/EventBookingSystemBackendApplication.java
+++ event-booking-system-backend/src/main/java/com/areeb/event_booking_system_backend/EventBookingSystemBackendApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EventBookingSystemBackendApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(EventBookingSystemBackendApplication.class, args);
 	}
 
