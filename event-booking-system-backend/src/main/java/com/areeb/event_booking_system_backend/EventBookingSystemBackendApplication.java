9c648d4fe5e11695ea8c4f8f8c29ff0afb7ba944
Eslam Samy <eslamsamy226@yahoo.com>
2025-05-05 21:25:16 +0300

    added backend project files

---
+++ event-booking-system-backend/src/main/java/com/areeb/event_booking_system_backend/EventBookingSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.areeb.event_booking_system_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventBookingSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventBookingSystemBackendApplication.class, args);
+	}
+
+}
---
+++ event-booking-system-backend/src/test/java/com/areeb/event_booking_system_backend/EventBookingSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.areeb.event_booking_system_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventBookingSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
